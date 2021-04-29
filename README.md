fb95953fac0503070fbd72e651f88cf8c08371ad
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2021-04-29 20:30:37 +0200

    Stuff and stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gnome/Ubuntu bootstraping script
+
+This script sets up my machines with sane defaults that I want them to have.
+The script should work with all machines that uses the `aptitude` package manager
+aswell as having access to the ubuntu ppas.
--- setup.sh
+++ setup.sh
@@ -1,43 +1,24 @@
 #!/usr/bin/env bash
 
+BOOTSTRAP_DIR=$HOME/.bootstrap
+
 installPackages() {
 	PACKAGES=(
 		build-essential
 		dconf-cli
 		vim
+		npm
+		nodejs
 		git
 		docker.io
 		golang
 	)
-	SNAP_PACKAGES_CLASSIC=(
-		slack
-	)
+	sudo apt update
 	sudo apt install -y ${PACKAGES[@]}
-	for PACKAGE in ${SNAP_PACKAGES_CLASSIC[@]}; do
-		yes | sudo snap install $PACKAGE --classic
-	done
 }
 
 setupGnomeTerminal() {
-	dconf load /org/gnome/terminal/legacy/profiles:/ << \
-EOF
-[:b1dcc9dd-5262-4d8d-a863-c897e6d979b9]
-audible-bell=false
-background-color='rgb(34,34,34)'
-bold-is-bright=false
-custom-command=''
-default-size-columns=120
-default-size-rows=70
-foreground-color='rgb(253,253,253)'
-login-shell=false
-palette=['rgb(34,34,34)', 'rgb(252,127,81)', 'rgb(90,215,110)', 'rgb(254,222,157)', 'rgb(236,130,212)', 'rgb(253,161,139)', 'rgb(103,209,251)', 'rgb(253,253,253)', 'rgb(146,137,134)', 'rgb(252,116,82)', 'rgb(114,254,154)', 'rgb(255,246,191)', 'rgb(253,164,233)', 'rgb(253,177,162)', 'rgb(155,226,253)', 'rgb(255,255,255)']
-preserve-working-directory='always'
-scrollbar-policy='never'
-use-custom-command=false
-use-system-font=true
-use-theme-colors=false
-visible-name='Andilutten'
-EOF
+	dconf load /org/gnome/terminal/legacy/profiles:/ < $BOOTSTRAP_DIR/terminal.dconf
 }
 
 installFonts() {
@@ -53,20 +34,11 @@ installFonts() {
 }
 
 setupInputSources() {
-	dconf load /org/gnome/desktop/input-sources/ << \
-EOF
-[/]
-sources=[('xkb', 'us+colemak')]
-xkb-options=['ctrl:nocaps', 'caps:none']
-EOF
+	dconf load /org/gnome/desktop/input-sources/ < $BOOTSTRAP_DIR/inputs.dconf
 }
 
 setupWindowManagement() {
-	dconf load /org/gnome/desktop/input-sources/ << \
-EOF
-[/]
-resize-with-right-button=true
-EOF
+	dconf load /org/gnome/desktop/input-sources/ < $BOOTSTRAP_DIR/windows.dconf
 }
 
 setupBash() {
